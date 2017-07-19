77e7374dead057f7041cd9ce591a657d0c4de0b3
nissablagojevic <nissa.blagojevic@gmail.com>
2017-07-20 01:12:14 +1000

    remove App.css import

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import {
     Component,
     } from "react";
 
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import {
     BrowserRouter as Router,
     Route,
     } from "react-router-dom";
-
 
 import {
     Navigation,
