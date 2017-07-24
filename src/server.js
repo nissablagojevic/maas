a16c5d56744e115b01e290f717b1e5d071baf392
nissablagojevic <nissa.blagojevic@gmail.com>
2017-07-24 12:07:35 +1000

    maybe in express instead

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
-#!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(3000, 'localhost');
-console.log('Server running at http://localhost:3000/');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3000);
