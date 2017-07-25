8875117efc3106dba6fabf42b7520539cde95297
Nissa Blagojevic <nissa.blagojevic@gmail.com>
2017-07-25 14:15:37 +1000

    server definitely serves build

--- server.js
+++ server.js
@@ -4,10 +4,4 @@ const app = express();
 
 app.use('/maas', express.static(path.join(__dirname, 'client/build')));
 
-app.get('/maas', function (req, res) {
-    console.log(req);
-    console.log(res);
-
-});
-
 app.listen(3000);
