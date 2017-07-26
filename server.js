6bd09cd52e855d54675bbcf0944bc0bad3f7ddd3
nissablagojevic <nissa.blagojevic@gmail.com>
2017-07-26 16:46:16 +1000

    Service worker should no longer provide problems on my local

--- server.js
+++ server.js
@@ -3,25 +3,11 @@ const path = require('path');
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
-    app.use('/', function (err, req, res, next) {
-        console.log(process.env.NODE_ENV);
-        express.static(path.join(__dirname, 'client/build'))
-    });
-    app.use('/client/build', function (err, req, res, next) {
-        console.log(process.env.NODE_ENV);
-        express.static(path.join(__dirname, 'client/build'))
-    });
+    app.use('/', express.static(path.join(__dirname, 'client/build')));
+    app.use('/*', express.static(path.join(__dirname, 'client/build')));
 }
 else {
-    app.use('/maas', function (err, req, res, next) {
-        console.log(process.env.NODE_ENV);
-        express.static(path.join(__dirname, 'client/build'))
-    });
-    app.use('/maas/client/build', function (err, req, res, next) {
-        console.log(process.env.NODE_ENV);
-        express.static(path.join(__dirname, 'client/build'))
-    });
+    app.use('/maas', express.static(path.join(__dirname, 'client/build')));
+    app.use('/maas/*', express.static(path.join(__dirname, 'client/build')));
 }
-
-
 app.listen(3000);
